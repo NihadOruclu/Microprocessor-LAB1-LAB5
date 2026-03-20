2e1f04756feb3b10b7f1f6d1026d1895a76ee364
Nihad Oruclu <norujlu19867@ada.edu.az>
2026-03-20 16:59:30 +0400

    Update Lab1 With DigitalWrite.ino

--- Lab/Lab1 With DigitalWrite.ino
+++ Lab/Lab1 With DigitalWrite.ino
@@ -1,12 +1,14 @@
-const int pin = 13; 
+const int pin = 13;      // Defining pin 13 for LED output (built-in LED on Arduino Uno).
 
-void setup() {
-  pinMode(pin, OUTPUT);
+void setup() { //Initialization part. For other codes I wont be including basic information as it is self explantory.
+  pinMode(pin, OUTPUT);   // Setting pin 13 as OUTPUT → allows us to control the LED state.
 }
 
-void loop() {
-  digitalWrite(pin, HIGH);
-  delay(500);
-  digitalWrite(pin, LOW);
-  delay(500);
+void loop() { //Loop part. 
+  digitalWrite(pin, HIGH); // Setting pin 13 HIGH → LED turns ON.
+  delay(500);              // Waits for 500 milliseconds (half a second)
+
+  digitalWrite(pin, LOW);  // Set pin 13 LOW → LED turns OFF.
+  delay(500);              // Waits for 500 milliseconds.
+  // 500 ms ON + 500 ms OFF → 1-second total cycle → 1 Hz blinking.
 }
