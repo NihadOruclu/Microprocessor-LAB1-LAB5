4e78b06581fdbc883c10e25f74efadf1e9d67cbf
Nihad Oruclu <norujlu19867@ada.edu.az>
2026-03-20 16:47:35 +0400

    Update Lab1 With AVR.cpp Commented version

--- Lab/Lab1 With AVR.cpp
+++ Lab/Lab1 With AVR.cpp
@@ -1,13 +1,14 @@
-const int pin = 5;
+const int pin = 5;       // Defining pin 5 as (PORTB5) for LED output which is actually PIN 13.
 
 void setup() {
-  DDRB |= (1 << pin);  
+  DDRB |= (1 << pin);    // Setting pin 5 of PORTB as output Data Direction Register B || 1 = output, 0 = input
 }
 
 void loop() {
-  PORTB |= (1 << pin);   
-  delay(500);          
+  PORTB |= (1 << pin);   // Setting pin 5 HIGH → LED turns ON
+  delay(500);            // Waits for 500 milliseconds
 
-  PORTB ^= (1 << 5);  
-  delay(500);          
+  PORTB ^= (1 << 5);     // Toggling pin 5 using (XOR) → LED changes state (ON → OFF or OFF → ON).
+  delay(500);            // Waits for 500 milliseconds 
+  //500+500 = 1HZ
 }
