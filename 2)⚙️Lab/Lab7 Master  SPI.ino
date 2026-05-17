a4ea293507f10d9be2dcaefa54eab6d5397ee3f6
Nihad Oruclu <norujlu19867@ada.edu.az>
2026-05-18 01:13:21 +0400

    Update Lab7 Master  SPI.ino

--- 2)⚙️Lab/Lab7 Master  SPI.ino
+++ 2)⚙️Lab/Lab7 Master  SPI.ino
@@ -1,38 +1,37 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-uint8_t dataSequence[3] = {85, 170, 255};
-uint8_t indexTx = 0;
+uint8_t dataSequence[3] = {85, 170, 255}; //Our values that will be transmitted via SPI.
+uint8_t indexTx = 0; //Index  used to cycle through the values within our array.
 
 void SPI_MasterInit() {
-    DDRB |= (1 << PB2) | (1 << PB3) | (1 << PB5);
-    // PB4(MISO) -> Input
-    DDRB &= ~(1 << PB4);
-    PORTB |= (1 << PB2);
+    DDRB |= (1 << PB2) | (1 << PB3) | (1 << PB5); //Setting PB2(SS), PB3(MOSI), PB5(SCK) as OUTPUT for SPI Master.
+    DDRB &= ~(1 << PB4); //Making sure MISO is input since Master receives data only.
+    PORTB |= (1 << PB2); //Setting SS HIGH initially (no slave selected at start).
 
-    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0);
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0); // Enabling SPI, setting Master mode, and setting clock prescaler fosc/128.
 }
 
 void SPI_MasterTransmit(uint8_t data) {
-    PORTB &= ~(1 << PB2); // SS LOW (Slave-i seç)
-    _delay_us(5);         // Qısa sinxronizasiya boşluğu
+    PORTB &= ~(1 << PB2); //Pulling SS LOW to select slave and start SPI transaction.
+    _delay_us(5); // Just a small delay to stabilize slave selection before transmission.
 
-    SPDR = data;          // Məlumatı göndər
-    while (!(SPSR & (1 << SPIF))); // Bitməsini gözlə
+    SPDR = data; //Loading data into SPI Data Register.
+    while (!(SPSR & (1 << SPIF))); //Waiting until SPI transmission is complete (SPIF flag set).
 
-    _delay_us(5);         // Slave-in qəbul etməsinə vaxt tanı
-    PORTB |= (1 << PB2);  // SS HIGH (Bitir)
+    _delay_us(5); //Simply a small delay making sure  slave has time to process received byte.
+    PORTB |= (1 << PB2); //Pulling SS HIGH to end SPI communication session.
 }
 
 void setup() {
-    SPI_MasterInit();
+    SPI_MasterInit(); //Initializing SPI in Master mode with configured pins and clock.
 }
 
 void loop() {
-    SPI_MasterTransmit(dataSequence[indexTx]);
-    
-    indexTx++;
-    if (indexTx >= 3) indexTx = 0;
+    SPI_MasterTransmit(dataSequence[indexTx]); // ending current value from array via SPI.
 
-    _delay_ms(500); // 1 saniyə çoxdur, 500ms daha dinamik görünür
+    indexTx++; //Moving to next value in sequence.
+    if (indexTx >= 3) indexTx = 0; //Resetting index to loop through values 85 → 170 → 255 continuously.
+
+    _delay_ms(500); //Another delay just this one is in between transmissions for visible output on oscilloscope and our serial monitor.
 }
