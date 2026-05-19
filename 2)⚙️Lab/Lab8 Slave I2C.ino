5dfb1db203d772a4a93122ce4181196c976af3b8
Nihad Oruclu <norujlu19867@ada.edu.az>
2026-05-19 20:00:54 +0400

    Update Lab8 Slave I2C.ino

--- 2)⚙️Lab/Lab8 Slave I2C.ino
+++ 2)⚙️Lab/Lab8 Slave I2C.ino
@@ -1,45 +1,45 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
 
-#define SLAVE_ADDR 0x08
+#define SLAVE_ADDR 0x08 
 
-volatile uint8_t state = 0x00 ;
+volatile uint8_t state = 0x00 ; //Value to send back to Master. OX08 will be our slave adress.
 
 void setup() {
-  TWAR = (SLAVE_ADDR << 1); // set address to the SLAVE_ADDR. Offset by 1 bit because LSM of TWAR is GSE.
-  TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT); // Enable TWI, send ACK, write 1 to TWINT to continue
+  TWAR = (SLAVE_ADDR << 1); //Setting slave address (shift because last bit reserved).
+  TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT); //Enabling TWI + ACK + clear flag.
 
-  PORTC |= (1 << PC4) | (1 << PC5);
+  PORTC |= (1 << PC4) | (1 << PC5); //Pullups for SDA and SCL.
 
-  DDRB |= (1 << PB5); 
-  DDRD &= ~(1 << PD2); 
-  PORTD |= (1 << PD2);
+  DDRB |= (1 << PB5); //LED output.
+  DDRD &= ~(1 << PD2); //Button input.
+  PORTD |= (1 << PD2); //Pull-up for button.
 }
 
 void loop() {
-      if (!(PIND & (1 << PD2))) { // check if button is pressed
-          state = 0x02;
+      if (!(PIND & (1 << PD2))) { //If button pressed (LOW).
+          state = 0x02; //Preparing value to send to Master.
       }
 
-      if (TWCR & (1 << TWINT)) {
-          uint8_t status = TWSR & 0b11111000; 
+      if (TWCR & (1 << TWINT)) { //Checking if I2C event happened.
+          uint8_t status = TWSR & 0b11111000; //Getting status code only.
 
-          if (status == 0x60) { // Master mənə müraciət etdi və nəsə yazmaq istəyir
-              TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT);
-          } else if (status == 0x80) { // Master mənə bir bayt məlumat göndərdi". Biz bu baytı TWDR-dən oxuyuruq. Əgər 0x01-dirsə, LED-i yandırırıq
-              uint8_t received = TWDR; // read TWDR
+          if (status == 0x60) { //Master wants to send data.
+              TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT); //Ready to receive.
+          } else if (status == 0x80) { //Data received from Master.
+              uint8_t received = TWDR; //Read data.
               if (received == 0x01) {  
-                  PORTB |= (1 << PB5);
+                  PORTB |= (1 << PB5); //LED ON.
               } else {
-                  PORTB &= ~(1 << PB5);
+                  PORTB &= ~(1 << PB5); //LED OFF.
               }
+              TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT); //Continue.
+          } else if (status == 0xA8) { //Master wants to read.
+              TWDR = state; //Send state.
               TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT);
-          } else if (status == 0xA8) { // Master mənə müraciət etdi və məndən nəsə oxumaq (Read) istəyir
-              TWDR = state; // send current state
-              TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT);
-              state = 0x00; 
-          } else { // in any other signal, ignore it, send ACK to continue communication and set TWINT to 1 to proceed
-              TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT);
+              state = 0x00; //Reset after sending.
+          } else { //Anything else.
+              TWCR = (1 << TWEN) | (1 << TWEA) | (1 << TWINT); //Ignore and continue.
           }
       }
 }
